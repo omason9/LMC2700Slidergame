a2744ce79dc41a8ac9983181153738ce34a1c1df
omason9 <114450076+omason9@users.noreply.github.com>
2022-12-07 17:24:09 -0500

    added scaling difficulty function, still need to implement levels and proper win tracking for higher difficulties

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let emptyR = 2;
 let emptyC = 2;
+scaledTileSize = 200;
 let movesCounter = 0;
 let tileGrid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 const correctTiles = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
@@ -18,8 +19,8 @@ var moveTile = function (tile, i, j) {
       //if possible move tile to empty space
       if (rOffset == 1 && cOffset == 0 || rOffset == 0 && cOffset == 1) {
         movesCounter++;
-        tile.style.marginLeft = emptyC * 200 + 'px';
-        tile.style.marginTop = emptyR * 200 + 'px';
+        tile.style.marginLeft = emptyC * scaledTileSize + 'px';
+        tile.style.marginTop = emptyR * scaledTileSize + 'px';
         //swap coordinates
         [row, emptyR] = [emptyR, row];
         [col, emptyC] = [emptyC, col];
@@ -28,10 +29,6 @@ var moveTile = function (tile, i, j) {
         var temp = tileGrid[emptyR][emptyC];
         tileGrid[emptyR][emptyC] = tileGrid[row][col];
         tileGrid[row][col] = temp;
-
-        console.log(tileGrid);
-        console.log(correctTiles);
-        console.log(checkVictory());
 
         if (gameRunning && checkVictory()) {
           alert("Congradulations, you completed the puzzle in " + movesCounter + " moves!");
@@ -66,7 +63,7 @@ function checkVictory() {
     gameRunning = true;
   };
   
-  var initTiles = function () {
+  function initTiles() {
     //get all rows
     var rows = document.querySelectorAll('.row');
   
@@ -91,12 +88,74 @@ function checkVictory() {
       }
     }
   };
+
+  function initTilesScalable(difficulty) {
+    //Indentify the puzzle div
+    var puzzle = document.querySelector('.puzzle');
+
+    console.log(difficulty);
+    //create rows in puzzle div
+    for (var i = 0; i < difficulty; i++) {
+      var row = document.createElement("div");
+      row.classList.add("row");
+      puzzle.appendChild(row);
+    }
+
+
+    //get all rows
+    var rows = document.querySelectorAll('.row');
+
+    console.log(rows.length);
+
+    //interate through rows to add tiles
+    for (let i = 0; i < rows.length; ++i) {
+      var row = rows.item(i);
+      for (let k = 0; k < difficulty; k++) {
+        if (!(i == difficulty - 1 && k == difficulty - 1)) {
+          var tile = document.createElement("div");
+          tile.classList.add("tile");
+          row.appendChild(tile);
+        }
+      }
+    } 
+
+    //scale global position values for difficulty
+    scaledTileSize = 600/difficulty;
+    emptyC = difficulty - 1;
+    emptyR = difficulty - 1;
+    
+  
+    //iterate thru rows and tiles to add images to tiles
+    for (let i = 0; i < rows.length; ++i) {
+      var row = rows.item(i);
+
+      var tiles = row.querySelectorAll('.tile');
+      //go thru each tile on each row
+      for (let j = 0; j < tiles.length; ++j) {
+        console.log(tiles.length);
+        var tile = tiles.item(j);
+
+        tile.style.width = (scaledTileSize - 1) + "px";
+        tile.style.height = (scaledTileSize -1) + "px";
+
+        //add the click response to tile
+        tile.addEventListener('click', moveTile(tile, i, j));
+
+        //set tile locations
+        tile.style.marginLeft = j * scaledTileSize + 'px';
+        tile.style.marginTop = i * scaledTileSize + 'px';
+  
+        // set what part of image will show on tile
+        tile.style.backgroundPosition = `${600 - j * scaledTileSize}px ${600 - i * scaledTileSize}px`;
+      }
+    }
+  };
   
   function initializeGame() {
-    var restartButtton = document.querySelector('.button1');
-    restartButtton.addEventListener('click', shuffle());
+    //var restartButtton = document.querySelector('.button1');
+    //restartButtton.addEventListener('click', shuffle());
 
-    initTiles();
+    initTilesScalable(5);
     shuffle();
   }
 
