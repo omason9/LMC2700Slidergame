ba6dfa879af41d697eec434fe4c53f274963332c
omason9 <114450076+omason9@users.noreply.github.com>
2022-12-08 23:09:10 -0500

    finished

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ let emptyR = 2;
 let emptyC = 2;
 scaledTileSize = 200;
 let movesCounter = 0;
-let tileGrid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-const correctTiles = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let tileGrid = [];
+let correctTiles = [];
 let gameRunning = false;
+let level = 3;
+let nextButton = document.getElementById("nextButton");
+let levelBeaten = false;
 
 var moveTile = function (tile, i, j) {
     var row = i;
@@ -13,6 +16,7 @@ var moveTile = function (tile, i, j) {
   
     //when a tile is clicked
     return function () {
+      console.log(correctTiles[0][0]);
       var rOffset = Math.abs(emptyR - row);
       var cOffset = Math.abs(emptyC - col);
   
@@ -32,6 +36,10 @@ var moveTile = function (tile, i, j) {
 
         if (gameRunning && checkVictory()) {
           alert("Congradulations, you completed the puzzle in " + movesCounter + " moves!");
+          nextButton.innerHTML = "Next Level!";
+          nextButton.classList.add('nextLevelEnabled');
+          levelBeaten = true;
+
         }
         
       }
@@ -40,6 +48,7 @@ var moveTile = function (tile, i, j) {
 
 //determines if all the tiles are in the correct place
 function checkVictory() {
+  console.log(correctTiles[0][0]);
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
       if (tileGrid[i][j] != correctTiles[i][j]) {
@@ -47,13 +56,14 @@ function checkVictory() {
       }
     }
   }
+  console.log(correctTiles[0][0]);
   return true;
 };
   
   function shuffle() {
     gameRunning = false;
     var rows = document.querySelectorAll('.row');
-    for (let i = 0; i < 85; ++i) {
+    for (let i = 0; i < level*50; ++i) {
       var row = ~~(Math.random() * rows.length);
       var tiles = rows.item(row).querySelectorAll('.tile');
       var tile = ~~(Math.random() * tiles.length);
@@ -67,7 +77,7 @@ function checkVictory() {
     //Indentify the puzzle div
     var puzzle = document.querySelector('.puzzle');
 
-    console.log(difficulty);
+    
     //create rows in puzzle div
     for (var i = 0; i < difficulty; i++) {
       var row = document.createElement("div");
@@ -79,7 +89,7 @@ function checkVictory() {
     //get all rows
     var rows = document.querySelectorAll('.row');
 
-    console.log(rows.length);
+    
 
     //interate through rows to add tiles
     for (let i = 0; i < rows.length; ++i) {
@@ -106,7 +116,7 @@ function checkVictory() {
       var tiles = row.querySelectorAll('.tile');
       //go thru each tile on each row
       for (let j = 0; j < tiles.length; ++j) {
-        console.log(tiles.length);
+        
         var tile = tiles.item(j);
 
         tile.style.width = (scaledTileSize - 1) + "px";
@@ -124,15 +134,62 @@ function checkVictory() {
       }
     }
   };
+
+
+  var generateCorrectPattern = function (size) {
+    var grid = [];
+    for (var i = 0; i < size; i++) {
+      var temprow = [];
+      for (var k = 0; k < size; k++) {
+        temprow[k] = k + 1 + i * size;
+      }
+      grid[i] = temprow;
+    }
+    return grid;
+  }
+
+  var nextLevel = function() {
+    return function() {
+      if(levelBeaten) {
+        level++;
+        clearTiles();
+        initializeGame();
+        nextButton.innerHTML = "Locked";
+        nextButton.classList.remove('nextLevelEnabled');
+        levelBeaten = false;
+      }
+    }
+    
+    
+  }
+
+
+  function clearTiles() {
+    var tiles = document.querySelectorAll('.tile');
+    for (var i = 0; i < tiles.length; i++) {
+      tiles.item(i).remove();
+    }
+    var rows = document.querySelectorAll('.row');
+    for (var i = 0; i < rows.length; i++) {
+      rows.item(i).remove();
+    }
+  }
+
   
   function initializeGame() {
     //currently commented out this stuff b/c it doesnt work
     //var restartButtton = document.querySelector('.button1');
     //restartButtton.addEventListener('click', shuffle());
-
-    initTilesScalable(5);
+    
+    tileGrid = generateCorrectPattern(level);
+    correctTiles = generateCorrectPattern(level);
+    console.log(correctTiles[0][0]);
+    initTilesScalable(level);
     shuffle();
   }
 
 
+
+//generateCorrectPattern(4);
+nextButton.addEventListener('click', nextLevel());
 initializeGame();
